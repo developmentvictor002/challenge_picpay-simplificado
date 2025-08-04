29eac1b7d85c84f04704220b0f33b9bc47fc15ac
Victor Henrique <developmentuser002@gmail.com>
2025-08-04 18:39:05 -0300

    feat: Create UserRequestDto with validations

---
+++ src/main/java/br/com/victor/picpay_simplificado/dto/UserRequestDto.java
@@ -0,0 +1,24 @@
+package br.com.victor.picpay_simplificado.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDto(
+        @NotBlank(message = "The full name field is required.")
+        @Size(min = 3, max = 100, message = "The full name must be between 3 and 100 characters.")
+        String fullName,
+
+        @NotBlank(message = "The CPF field is required.")
+        String cpf,
+
+        @NotBlank(message = "The email field is required.")
+        @Size(max = 100, message = "The email must be at most 100 characters.")
+        @Email(message = "Invalid email format.")
+        String email,
+
+        @NotBlank(message = "The password field is required.")
+        @Size(min = 6, message = "The password must be at least 6 characters long")
+        String password
+) {
+}
