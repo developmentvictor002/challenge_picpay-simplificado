0ec369e510bb13f41549dc2a171943510c744919
Victor Henrique <developmentuser002@gmail.com>
2025-08-04 21:59:42 -0300

    feat: create custom exception for email already exists (EmailAlreadyExistsException)

---
+++ src/main/java/br/com/victor/picpay_simplificado/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package br.com.victor.picpay_simplificado.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String email) {
+        super("Email already in use: " + email);
+    }
+}
