95d926c986051d804692a05fb18c9faf86783c5c
Victor Henrique <developmentuser002@gmail.com>
2025-08-04 22:18:24 -0300

    feat: add global exception handler for UserNotFoundException

--- src/main/java/br/com/victor/picpay_simplificado/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/victor/picpay_simplificado/exception/GlobalExceptionHandler.java
@@ -1,8 +1,16 @@
 package br.com.victor.picpay_simplificado.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 
 }
