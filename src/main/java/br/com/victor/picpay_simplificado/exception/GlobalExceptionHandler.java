1cd9d2648e35eed6b12bc0bd89975def630776f0
Victor Henrique <developmentuser002@gmail.com>
2025-08-04 23:13:06 -0300

    feat: add custom exception handling for invalid UUID format in user endpoint

--- src/main/java/br/com/victor/picpay_simplificado/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/victor/picpay_simplificado/exception/GlobalExceptionHandler.java
@@ -1,16 +1,46 @@
 package br.com.victor.picpay_simplificado.exception;
 
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ProblemDetail> handleUserNotFound(UserNotFoundException ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setTitle("User not found");
+        problemDetail.setDetail(ex.getMessage());
+        URI instanceUri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .build()
+                .toUri();
+        problemDetail.setInstance(instanceUri);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    }
+
+    @ExceptionHandler({ MethodArgumentTypeMismatchException.class })
+    public ResponseEntity<ProblemDetail> handleInvalidUUID(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        if (ex.getRequiredType().equals(UUID.class)) {
+            ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+            problemDetail.setTitle("Invalid UUID format");
+            problemDetail.setDetail("The provided UUID value is invalid.");
+            URI instanceUri = URI.create(request.getDescription(false));
+            problemDetail.setInstance(instanceUri);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+        }
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
     @ExceptionHandler({EmailAlreadyExistsException.class, CpfAlreadyExistsException.class})
